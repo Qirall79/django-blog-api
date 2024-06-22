c46a3848ee2ca7f13083ca4feedec9b04febc4cb
qirall79 <belfatmi79@gmail.com>
2024-06-22 19:24:56 +0100

    Blog API setup

--- posts/tests.py
+++ posts/tests.py
@@ -9,7 +9,7 @@ class BlogTests(TestCase):
         cls.user = get_user_model().objects.create(
             username="chrollo", email="chrollo@gmail.com", password="chrollo123")
         cls.post = Post.objects.create(title="test post", body="This is test body", author=cls.user)
-        
+
     def test_post_model(self):
         self.assertEqual(self.post.author.username, "chrollo")
         self.assertEqual(self.post.title, "test post")
